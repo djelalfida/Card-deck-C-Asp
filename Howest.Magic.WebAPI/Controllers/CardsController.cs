b940bb9034ba6492ca466ca96de98c2daba93ee4
Djelal Fida <djelal.fida@student.howest.be>
2022-05-26 22:41:16 +0200

    Sending the paged result

--- Howest.Magic.WebAPI/Controllers/CardsController.cs
+++ Howest.Magic.WebAPI/Controllers/CardsController.cs
@@ -1,6 +1,9 @@
 ﻿using Howest.MagicCards.DAL.Models; // fix later
 using Howest.MagicCards.DAL.Repositories; // fix later
 using Howest.MagicCards.Shared.DTO;
+using Howest.MagicCards.Shared.Extensions;
+using Howest.MagicCards.Shared.Filters;
+using Howest.MagicCards.WebAPI.Wrappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Howest.MagicCards.WebAPI.Controllers
@@ -9,22 +12,41 @@ namespace Howest.MagicCards.WebAPI.Controllers
     [ApiController]
     public class CardsController : Controller
     {
-        private readonly ICardRepository _cardRepository;
+        private readonly ICardRepository _cardRepo;
         private readonly IMapper _mapper;
 
         public CardsController(ICardRepository cardRepository, IMapper mapper)
         {
-            _cardRepository = cardRepository;
+            _cardRepo = cardRepository;
             _mapper = mapper;
         }
 
-        // get all cards endpoint and automap to CardDTO
+
         [HttpGet]
-        public ActionResult<IEnumerable<CardReadDTO>> GetAllCards()
+        public ActionResult<PagedResponse<IEnumerable<CardReadDTO>>> GetAllCards([FromQuery] PaginationFilter filter, [FromServices] IConfiguration config)
         {
-            var cards = _cardRepository.GetAllCards();
-            var cardsDTO = _mapper.Map<IEnumerable<CardReadDTO>>(cards);
-            return Ok(cardsDTO);
+
+            filter.MaxPageSize = int.Parse(config["maxPageSize"]);
+            
+            return (_cardRepo.GetAllCards() is IQueryable<Card> allCards)
+                   ? Ok(new PagedResponse<IEnumerable<CardReadDTO>>(
+                           allCards
+                               .ToPagedList(filter.PageNumber, filter.PageSize)
+                               .ProjectTo<CardReadDTO>(_mapper.ConfigurationProvider)
+                               .ToList(),
+                           filter.PageNumber,
+                           filter.PageSize)
+                   {
+                       TotalRecords = allCards.Count()
+                   })
+                   : NotFound(new Response<CardReadDTO>()
+                   {
+                       Succeeded = false,
+                       Errors = new string[] { "404" },
+                       Message = "No cards found "
+                   }
+                   );
+
         }
 
 
