f3d298d192ab67980611e615b3b4541365fa50f6
Djelal Fida <djelal.fida@student.howest.be>
2022-05-26 21:50:05 +0200

    Generate MTG context scaffold

--- Howest.Magic.DAL/Models/mtg_v1Context.cs
+++ Howest.Magic.DAL/Models/mtg_v1Context.cs
@@ -27,14 +27,14 @@ namespace Howest.MagicCards.DAL.Models
         public virtual DbSet<Set> Sets { get; set; } = null!;
         public virtual DbSet<Type> Types { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=mtg_v1;Integrated Security=True;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=mtg_v1;Integrated Security=True;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
