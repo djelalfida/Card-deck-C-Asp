36cb89179b6fbc930f0b02d64ab181890e4866f9
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 18:48:44 +0200

    Setting up GraphQL server

--- Howest.MagicCards.GraphQL/Program.cs
+++ Howest.MagicCards.GraphQL/Program.cs
@@ -1,6 +1,30 @@
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager config = builder.Configuration;
+
+builder.Services.AddDbContext<mtg_v1Context>
+    (options => options.UseSqlServer(config.GetConnectionString("mtg_v1")));
+builder.Services.AddScoped<ICardRepository, CardRepository>();
+
+builder.Services.AddScoped<RootSchema>();
+builder.Services.AddGraphQL()
+                .AddGraphTypes(typeof(RootSchema), ServiceLifetime.Scoped)
+                .AddDataLoader()
+                .AddSystemTextJson();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseGraphQL<RootSchema>();
+app.UseGraphQLPlayground(options: new GraphQLPlaygroundOptions()
+{
+    EditorTheme = EditorTheme.Light
+}
+);
+
+
 
 app.Run();
+
