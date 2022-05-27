fe761c0486d7950d5f460f0429ff7e5d2214dbd3
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 21:09:35 +0200

    Filter on power and toughness

--- Howest.MagicCards.GraphQL/GraphQL/Query/RootQuery.cs
+++ Howest.MagicCards.GraphQL/GraphQL/Query/RootQuery.cs
@@ -1,4 +1,6 @@
-﻿namespace Howest.MagicCards.GraphQL.GraphQLTypes;
+﻿using GraphQL;
+
+namespace Howest.MagicCards.GraphQL.GraphQLTypes;
 
 public class RootQuery : ObjectGraphType
 {
@@ -8,9 +10,22 @@ public class RootQuery : ObjectGraphType
 
         Field<ListGraphType<CardType>>(
             "cards",
-            resolve: context => cardRepository
-                                .GetAllCards()
-                                .ToList()
+            arguments: new QueryArguments(
+                new QueryArgument<StringGraphType> { Name = "power" },
+                new QueryArgument<StringGraphType> { Name = "toughness" }
+            ),
+            resolve: context =>
+            {
+                string power = context.GetArgument<string>("power");
+                string toughness = context.GetArgument<string>("toughness");
+
+                return cardRepository
+                    .GetAllCards()
+                    .Where(c =>
+                        (power == null || c.Power == power) &&
+                        (toughness == null || c.Toughness == toughness))
+                    .ToList();
+            }
         );
     }
 }
