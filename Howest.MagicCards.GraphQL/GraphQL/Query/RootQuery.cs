444c70eafe672c7cbb9559349f31d93931b8b21e
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 18:47:35 +0200

    All cards graphql query

---
+++ Howest.MagicCards.GraphQL/GraphQL/Query/RootQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace Howest.MagicCards.GraphQL.GraphQLTypes;
+
+public class RootQuery : ObjectGraphType
+{
+    public RootQuery(ICardRepository cardRepository)
+    {
+        Name = "Query";
+
+        Field<ListGraphType<CardType>>(
+            "cards",
+            resolve: context => cardRepository
+                                .GetAllCards()
+                                .ToList()
+        );
+    }
+}
