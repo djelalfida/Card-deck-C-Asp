d678941a197dd8891a904a59c6cb3e8364aa77a3
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 18:46:01 +0200

    CardType type for GraphQL

---
+++ Howest.MagicCards.GraphQL/GraphQL/Types/CardType.cs
@@ -0,0 +1,27 @@
+﻿namespace Howest.MagicCards.GraphQL.GraphQLTypes;
+
+public class CardType: ObjectGraphType<Card>
+{
+    public CardType(ICardRepository cardRepository)
+    {
+        Name = "Card";
+
+        Field(c => c.Id, type: typeof(IdGraphType))
+            .Description("The id of the card")
+            .Name("Id");
+
+        Field(c => c.Name, type: typeof(StringGraphType))
+            .Description("The name of the card")
+            .Name("Name");
+
+        Field(c => c.ManaCost, type: typeof(StringGraphType))
+            .Description("The mana cost of the card")
+            .Name("ManaCost");
+
+        Field(c => c.ConvertedManaCost, type: typeof(StringGraphType))
+            .Description("The converted mana cost of the card")
+            .Name("ConvertedManaCost");
+
+
+    }
+}
