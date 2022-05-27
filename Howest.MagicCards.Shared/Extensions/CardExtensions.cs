f150e1a6e909387e845e6bdd726d40ad40aa3105
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 14:03:37 +0200

    Implementing filtering on Cards

---
+++ Howest.MagicCards.Shared/Extensions/CardExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared.Extensions;
+
+public static class CardExtensions
+{
+    public static IQueryable<Card> ToFilteredList(this IQueryable<Card> cards, string setCode, int artistId, string rarityCode, string name, string originalText)
+    {
+        if (!string.IsNullOrEmpty(setCode))
+        {
+            cards = cards.Where(c => c.SetCode == setCode);
+        }
+
+        if (artistId > 0)
+        {
+            cards = cards.Where(c => c.ArtistId == artistId);
+        }
+
+        if (!string.IsNullOrEmpty(rarityCode))
+        {
+            cards = cards.Where(c => c.RarityCode == rarityCode);
+        }
+
+        if (!string.IsNullOrEmpty(name))
+        {
+            cards = cards.Where(c => c.Name.Contains(name));
+        }
+
+        if (!string.IsNullOrEmpty(originalText))
+        {
+            cards = cards.Where(c => c.OriginalText.Contains(originalText));
+        }
+
+        return cards;
+    }
+}
