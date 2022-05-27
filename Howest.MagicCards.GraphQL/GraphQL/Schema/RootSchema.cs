4580edb5c0ced5fad9ef98c9196a3ead501b8bab
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 18:48:14 +0200

    RootSchema creation for GraphQL

---
+++ Howest.MagicCards.GraphQL/GraphQL/Schema/RootSchema.cs
@@ -0,0 +1,9 @@
+﻿namespace Howest.MagicCards.GraphQL.GraphQLTypes;
+
+public class RootSchema: Schema
+{
+    public RootSchema(IServiceProvider provider) : base(provider)
+    {
+        Query = provider.GetRequiredService<RootQuery>();
+    }
+}
