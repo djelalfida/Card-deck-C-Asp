0872c3b7661e06a46839a2b2dbf660f9afa64d2e
Djelal Fida <djelal.fida@student.howest.be>
2022-05-27 14:33:46 +0200

    Added sort in CardFilter

--- Howest.MagicCards.Shared/Filters/CardFilter.cs
+++ Howest.MagicCards.Shared/Filters/CardFilter.cs
@@ -8,17 +8,18 @@ namespace Howest.MagicCards.Shared.Filters;
 
 public class CardFilter : PaginationFilter
 {
-
-    public string SetCode { get; init; } = String.Empty;
+    
+    public string SetCode { get; init; } = string.Empty;
     public int ArtistId { get; init; } = 0;
-    public string RarityCode { get; init; } = String.Empty;
-    public string Name { get; init; } = String.Empty;
-    public string OriginalText { get; init; } = String.Empty;
+    public string RarityCode { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public string OriginalText { get; init; } = string.Empty;
+    public string? Sort { get; init; }
 
     // create toString
     public override string ToString()
     {
-        return $"PageSize: {base.PageSize}, PageNumber: {base.PageNumber}, MaxPageSize: {base.MaxPageSize}, SetCode: {SetCode}, ArtistId: {ArtistId}, RarityCode: {RarityCode}, Name: {Name}, OriginalText: {OriginalText}";
+        return $"PageSize: {base.PageSize}, PageNumber: {base.PageNumber}, MaxPageSize: {base.MaxPageSize}, SetCode: {SetCode}, ArtistId: {ArtistId}, RarityCode: {RarityCode}, Name: {Name}, OriginalText: {OriginalText}, Sort: {Sort}";
     }
 
 }
