37e9b54607b002792361991bbd0cab4138cd341e
Djelal Fida <djelal.fida@student.howest.be>
2022-05-28 20:52:07 +0200

    DTO for reading artists

---
+++ Howest.MagicCards.Shared/DTO/Artist/ArtistReadDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared.DTO;
+
+public record ArtistReadDTO
+{
+    public long Id { get; init; }
+    public string FullName { get; init; }
+}
