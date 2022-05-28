b666d1e74ba831190e55e571cff938a47424b076
Djelal Fida <djelal.fida@student.howest.be>
2022-05-29 00:08:58 +0200

    Implement dto for rarity

---
+++ Howest.MagicCards.Shared/DTO/Card/RarityReadDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared.DTO;
+
+public record RarityReadDTO
+{
+    public long Id { get; init; }
+    public string? Code { get; init; }
+    public string? Name { get; init; }
+}
